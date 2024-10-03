3056d6f4f57b012cb6516c06cac8abcb6d9effe9
shravan kumar <kumashravan5@gmail.com>
2024-10-04 00:05:02 +0530

    dashboard koncham done

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import React, { useEffect, useState } from "react";
 import { BACKEND_URL, HOOKS_URL } from "../config";
 import axios from "axios";
--- frontend/app/zap/create/page.tsx
+++ frontend/app/zap/create/page.tsx
@@ -1,18 +1,56 @@
-export default function(){
-    return <div className="w-full h-screen bg-slate-200">
-        <div className="mx-auto max-w-lg">
-        <Sandbox />
+"use client";
+import { ZapCell } from "@/components/zapcell";
+import { useState } from "react";
 
-        </div>
-        
-    </div>;
-}
-
-function Sandbox(){
-    return <div className="w-full max-h-fit bg-red-200">
-
-<div className="bg-gray-300 p-7">
-    create a triger
-</div>
+export default function () {
+  const [selectedTrigger, setSelectedTrigger] = useState("");
+  const [selectedActions, setSelectedActions] = useState<
+    {
+      availableActionId: string;
+      availableActionName: string;
+    }[]
+  >([]);
+  return (
+    <div className="w-full  flex flex-col mx-auto  justify-center  h-screen  bg-slate-200">
+      <div className="justify-center mb-3 flex">
+        <ZapCell
+          onClick={() => {
+            console.log("helo");
+          }}
+          name={selectedTrigger ? selectedTrigger : "Trigger"}
+          index={1}
+        />
+      </div>
+      <div className=" flex-col  flex justify-center   mx-auto  ">
+        {selectedActions.map((x, index) => {
+          return (
+            <ZapCell
+            
+              key={index}
+              onClick={() => {
+                console.log("helo");
+              }}
+              name={x ? x.availableActionName : "action"}
+              index={index + 1}
+            />
+          );
+        })}
+      </div>
+      <button
+        className="text-4xl  font-semibold"
+        onClick={() => {
+          setSelectedActions((a) => [
+            ...a,
+            {
+              availableActionId: "",
+              availableActionName: "game",
+            },
+          ]);
+        }}
+        type="button"
+      >
+        +
+      </button>
     </div>
+  );
 }
